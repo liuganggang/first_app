8f532a7566ecf500793cb8cecd7d366089cd1945
liugang <501225658@qq.com>
2013-11-20 10:29:29 +0800

    init 4.0 project for learn tor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4d7f672aa25144e1526f588b81a54b941cb1ab5edc6fc939b7743a77bbc00aab55deec80dd0699d30de8168a2401ec6a0d29c36608a72ae64b67f4fb5e3d2181'
