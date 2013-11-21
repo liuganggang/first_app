7bf57eba208dfbc8deb2fbed1b05c93da1c6b4d0
liugang <501225658@qq.com>
2013-11-21 18:34:01 +0800

    add factory girl and ssl

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
-gem 'bootstrap-sass', '~> 3.0.2.0'
+gem 'bootstrap-sass', '2.3.2.0'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
@@ -19,6 +19,7 @@ end
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  gem 'factory_girl_rails', '4.2.1'
 end
 
 # add heroku server
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,39 @@ describe "UserPages" do
 
   describe "have a sign_up_page" do
     before { visit sign_up_path }
+
     it { should have_selector('h1', text: 'sign up') }
     it { should have_title(full_title('Sign_up')) }
+  end
+
+  describe "sugn_up." do
+    let(:submit) { "Create my account" }
+
+    before { visit sign_up_path }
+    describe "with invalid information" do
+      it "should not create a user." do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Emall",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      it "should create new user by fill_in." do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+
+  describe "show page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_selector('h3',text: user.emall) }
   end
 end
